15c2e05e0bb5cfb65d5c4940c9f5e8e3a734db6f
rardati601 <rardati601@g.educaand.es>
2024-10-27 21:13:05 +0100

    Actualizado

--- adivina/src/app/home/home.page.ts
+++ adivina/src/app/home/home.page.ts
@@ -5,8 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  num: number;
+  numSecret: number = this.numAleatorio(1, 100);
+
+  mayorMenor: string = '...';
+
+  constructor() {
+    console.log("El numero secreto es: " + this.numSecret);
+  }
+
+  numAleatorio(a, b) {
+    return Math.round(Math.random() * (a - b) + parseInt(a));
+  }
+
+  compruebaNumero() {
+    if (this.num) {
+      if (this.numSecret < this.num) {
+        this.mayorMenor = 'menor';
+      } else if (this.numSecret > this.num) {
+        this.mayorMenor = 'mayor';
+      } else {
+        this.mayorMenor = 'igual';
+      }
+    }
+  }
 
 }
